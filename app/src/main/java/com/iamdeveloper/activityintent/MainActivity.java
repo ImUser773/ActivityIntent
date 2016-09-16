866eae5d302fe0a49da114418a9bf4f75e7cddd4
IamDeveloper <iauser773@gmail.com>
2016-09-16 22:32:55 +0700

    Activity Intent

---
+++ app/src/main/java/com/iamdeveloper/activityintent/MainActivity.java
@@ -0,0 +1,35 @@
+package com.iamdeveloper.activityintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText input;
+    Button button;
+    String data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        input = (EditText) findViewById(R.id.input);
+        button = (Button) findViewById(R.id.btn_intent);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                data = input.getText().toString();
+                if(!data.isEmpty()){
+                    Intent intent = new Intent(MainActivity.this,Second_Activity.class);
+                    intent.putExtra("data",data);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        });
+    }
+}
